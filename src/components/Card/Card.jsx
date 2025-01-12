cf5421ed654824d61872edccff1a0b543aa10093
ZackCornfield <139100711+ZackCornfield@users.noreply.github.com>
2025-01-12 11:05:32 +0800

    [Add] Added Card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,27 @@
+import "./Card.css";    
+
+export default function Card({onCLick, characterId, characterName, characterImage}) {
+    return (
+        <div
+            className="card-container"
+            data-character-id={characterId} 
+            onClick={() => onClick()}
+        >
+            <div className="card">
+                <div className="card-front inner-card">
+                    <div className="card-front card-content">
+                        <img 
+                            className="card-character-image"
+                            src={characterImage}    
+                            alt={'character: ' + characterName}         
+                        />
+                        <p className='card-character-name'>{characterName}</p>  
+                    </div>
+                </div>
+                <div className="card-back inner-card">
+                    <div className="chip"></div>    
+                </div>
+            </div> 
+        </div>
+    )
+}
